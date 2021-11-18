2b763313b1558deb097ae841c721b69ad1e8b2e8
mamume <mmustafametwally@gmail.com>
2021-11-18 22:05:11 +0200

    Adding Main Theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        color: 'info'
+      }
+    },
+    MuiButton: {
+      defaultProps: {
+        size: "large",
+        variant: "contained",
+      }
+    }
+  },
+  palette: {
+    primary: {
+      main: red[700],
+      light: red[300],
+      dark: red[900]
+    }
+  },
+  shape: {
+    borderRadius: "24px"
+  }
+});
+
 function App() {
   return (
     <div>
-      Hello, World!
+      <ThemeProvider theme={theme}>
+      </ThemeProvider>
     </div>
   );
 }
