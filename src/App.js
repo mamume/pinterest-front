223fdd660f1ee81a571fcf28c402786917f0f2ee
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2021-11-19 00:42:48 +0200

    added create button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { red } from "@mui/material/colors";
 import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import PrimarySearchAppBar from './Components/NavigationBar'
+import { IconButton } from '@mui/material'; 
+import AddIcon from '@mui/icons-material/Add';
+
 
 const theme = createTheme({
   components: {
@@ -33,6 +36,21 @@ function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
+      <IconButton color="primary" aria-label="add to shopping cart" style={{
+        position: "fixed",
+        bottom: 90,
+        right: 50,
+        padding: "15px",
+        color: "black",
+        border: "0.02px solid black",
+        
+      }}>
+        <AddIcon sx={{
+          fontSize: 20,
+          transform: "scale(2)"
+
+        }}  />
+      </IconButton>
         <PrimarySearchAppBar></PrimarySearchAppBar>
       </ThemeProvider>
     </div>
