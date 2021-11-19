65902170c511b2b2df963bb5a5c954e861add7ee
mamume <mmustafametwally@gmail.com>
2021-11-19 14:47:09 +0200

    Initializing notifications component

---
+++ src/components/Notifications.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import { Fragment } from "react";
+
+function Notifications() {
+    return (
+        <Fragment>
+            <Typography variant="h5">Notifications</Typography>
+        </Fragment>
+    );
+}
+
+export default Notifications;
