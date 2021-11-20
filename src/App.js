ae6ced25e78f0f61e75d0bb2399d5f527a298523
mamume <mmustafametwally@gmail.com>
2021-11-20 12:15:36 +0200

    Renaming App.css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { red } from "@mui/material/colors";
 import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import PrimarySearchAppBar from './components/NavigationBar'
-import { IconButton } from '@mui/material'; 
+import { IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import './app.css'
+import './App.css'
 
 
 const theme = createTheme({
@@ -37,22 +37,19 @@ function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
-      <IconButton color="primary" aria-label="add to shopping cart" style={{
-        position: "fixed",
-        bottom: 90,
-        right: 50,
-        padding: "15px",
-        color: "black",
-        //border: "0.02px solid black",
-        
-        
-      }} sx={{boxShadow: 3,}}>
-        <AddIcon sx={{
-          fontSize: 20,
-          transform: "scale(2)"
+        <IconButton color="primary" aria-label="add to shopping cart" style={{
+          position: "fixed",
+          bottom: 90,
+          right: 50,
+          padding: "15px",
+          color: "black",
+        }} sx={{ boxShadow: 3, }}>
+          <AddIcon sx={{
+            fontSize: 20,
+            transform: "scale(2)"
 
-        }}  />
-      </IconButton>
+          }} />
+        </IconButton>
         <PrimarySearchAppBar></PrimarySearchAppBar>
       </ThemeProvider>
     </div>
