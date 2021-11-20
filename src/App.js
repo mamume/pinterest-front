10efbc0ab8a5638e29f03295d585de8ef8cb50a6
mamume <mmustafametwally@gmail.com>
2021-11-20 19:24:10 +0200

    Separte Theme

--- src/App.js
+++ src/App.js
@@ -1,79 +1,8 @@
-import { red } from "@mui/material/colors";
-import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import Settings from './pages/Settings'
-import { grey } from "@mui/material/colors";
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
-
-
-const theme = createTheme({
-  components: {
-    MuiInputBase: {
-      defaultProps: {
-        sx: {
-          borderRadius: "16px",
-          height: "48px"
-        },
-      }
-    },
-    MuiTextField: {
-      defaultProps: {
-        color: 'info',
-        InputLabelProps: {
-          sx: {
-            fontSize: "16px"
-          }
-        }
-      }
-    },
-    MuiButton: {
-      defaultProps: {
-        size: "large",
-        variant: "contained",
-        sx: {
-          borderRadius: "20px"
-        }
-      }
-    },
-    MuiSelect: {
-      defaultProps: {
-        color: 'info'
-      }
-    },
-    MuiInputLabel: {
-      defaultProps: {
-        color: 'info'
-      }
-    },
-    MuiRadio: {
-      defaultProps: {
-        color: 'black'
-      }
-    },
-    MuiFormLabel: {
-      defaultProps: {
-        color: 'black'
-      }
-    },
-    MuiCheckbox: {
-      defaultProps: {
-        color: 'black'
-      }
-    },
-  },
-  palette: {
-    primary: {
-      main: red[700],
-      light: red[300],
-      dark: red[900]
-    },
-    black: {
-      main: grey[900],
-      light: grey[500],
-    }
-  },
-});
+import theme from './theme/Theme'
 
 function App() {
   return (
---
+++ src/theme/Theme.js
@@ -0,0 +1,73 @@
+import { createTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
+import { grey } from "@mui/material/colors";
+
+const theme = createTheme({
+    components: {
+        MuiInputBase: {
+            defaultProps: {
+                sx: {
+                    borderRadius: "16px",
+                    height: "48px"
+                },
+            }
+        },
+        MuiTextField: {
+            defaultProps: {
+                color: 'info',
+                InputLabelProps: {
+                    sx: {
+                        fontSize: "16px"
+                    }
+                }
+            }
+        },
+        MuiButton: {
+            defaultProps: {
+                size: "large",
+                variant: "contained",
+                sx: {
+                    borderRadius: "20px"
+                }
+            }
+        },
+        MuiSelect: {
+            defaultProps: {
+                color: 'info'
+            }
+        },
+        MuiInputLabel: {
+            defaultProps: {
+                color: 'info'
+            }
+        },
+        MuiRadio: {
+            defaultProps: {
+                color: 'black'
+            }
+        },
+        MuiFormLabel: {
+            defaultProps: {
+                color: 'black'
+            }
+        },
+        MuiCheckbox: {
+            defaultProps: {
+                color: 'black'
+            }
+        },
+    },
+    palette: {
+        primary: {
+            main: red[700],
+            light: red[300],
+            dark: red[900]
+        },
+        black: {
+            main: grey[900],
+            light: grey[500],
+        }
+    },
+});
+
+export default theme;
