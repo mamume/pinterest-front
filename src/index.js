abf68156ee199e30de91864e657530f89d5cb7d0
mamume <mmustafametwally@gmail.com>
2021-11-22 12:31:15 +0200

    Fixing index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import Auth from './Auth/Auth';
+import App from './App';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,7 +8,7 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
