d54dc2231dbdbbae2afed70f3d21561b56a423dd
mamume <mmustafametwally@gmail.com>
2021-11-22 21:08:51 +0200

    Changing link styles in profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,47 +3,63 @@ import React, { Fragment } from "react";
 import MenuButton from '../components/settings/MenuButton'
 import AddRounded from "@mui/icons-material/AddRounded";
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { Link } from 'react-router-dom'
+import { makeStyles } from "@mui/styles";
 
+
+const useStyles = makeStyles({
+  link: {
+    textDecoration: "inherit",
+    color: "inherit",
+    '&:hover': {
+      textDecoration: "inherit",
+    }
+  },
+})
 
 function Profile() {
-    return (
-        <Fragment>
-            <Stack direction="column" alignItems="center">
-                <Avatar sx={{ width: 120, height: 120 }} size='large' alt="Profile Image">
-                    <Typography variant="h2">M</Typography>
-                </Avatar>
+  const classes = useStyles()
 
-                <Typography mt fontWeight="bold" variant="h4">Mahmoud Metwally</Typography>
-                <Typography>@username</Typography>
-                <Typography>[numOfFollowing] following</Typography>
+  return (
+    <Fragment>
+      <Stack direction="column" alignItems="center">
+        <Avatar sx={{ width: 120, height: 120 }} size='large' alt="Profile Image">
+          <Typography variant="h2">M</Typography>
+        </Avatar>
 
-                <Stack direction="row" spacing={1} mt>
-                    <Button disableElevation color="grey">Share</Button>
-                    <Button disableElevation color="grey">Edit Profile</Button>
-                </Stack>
-            </Stack>
+        <Typography mt fontWeight="bold" variant="h4">Mahmoud Metwally</Typography>
+        <Typography>@username</Typography>
+        <Typography>[numOfFollowing] following</Typography>
 
-            <Stack direction="row" justifyContent="space-between" mt={7}>
-                <MenuButton
-                    icon={<MenuRoundedIcon fontSize="large" />}
-                    label="Sort boards by"
-                    options={["A to Z", "Drag and drop", "Last saved to"]}
-                />
+        <Stack direction="row" spacing={1} mt>
+          <Button disableElevation color="grey">Share</Button>
+          <Link to="/settings" className={classes.link}>
+            <Button disableElevation color="grey">Edit Profile</Button>
+          </Link>
+        </Stack>
+      </Stack>
 
-                <MenuButton
-                    icon={<AddRounded fontSize="large" />}
-                    label="Create"
-                    options={["Pin", "Board"]}
-                />
-            </Stack>
+      <Stack direction="row" justifyContent="space-between" mt={7}>
+        <MenuButton
+          icon={<MenuRoundedIcon fontSize="large" />}
+          label="Sort boards by"
+          options={["A to Z", "Drag and drop", "Last saved to"]}
+        />
 
-            <Divider />
-            <Stack direction='row' justifyContent="space-between" mt={3}>
-                <Typography fontWeight="bold" variant="h6">Unorganized Ideas</Typography>
-                <Button color="grey">Organize</Button>
-            </Stack>
-        </Fragment >
-    );
+        <MenuButton
+          icon={<AddRounded fontSize="large" />}
+          label="Create"
+          options={["Pin", "Board"]}
+        />
+      </Stack>
+
+      <Divider />
+      <Stack direction='row' justifyContent="space-between" mt={3}>
+        <Typography fontWeight="bold" variant="h6">Unorganized Ideas</Typography>
+        <Button color="grey">Organize</Button>
+      </Stack>
+    </Fragment >
+  );
 }
 
 export default Profile;
