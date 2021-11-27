60fc0a4d221e47ada6082d2aca7d44577c86393e
mamume <mmustafametwally@gmail.com>
2021-11-27 14:44:52 +0200

    not found profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,7 @@ import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { Link } from 'react-router-dom'
 import { makeStyles } from "@mui/styles";
 import HomePage from "./Homepage"
+import NotFound from '../components/NotFound'
 
 
 const useStyles = makeStyles({
@@ -24,6 +25,7 @@ function Profile() {
   const [followingNum, setFollowingNum] = useState(0)
   const [profilePic, setProfilePic] = useState('')
   const [username, setUsername] = useState('')
+  const [notFound, setNotFound] = useState(false)
 
 
   useEffect(() => {
@@ -38,14 +40,16 @@ function Profile() {
       })
         .then(res => res.json())
         .then(data => {
-          console.log(data)
-          const { first_name, last_name, username, profile_pic, following } = data
-          setFullName(`${first_name} ${last_name}`)
-          setFollowingNum(following.length)
-          setProfilePic(profile_pic)
-          setUsername(username)
+          if (data.msg)
+            setNotFound(true)
+          else {
+            const { first_name, last_name, username, profile_pic, following } = data
+            setFullName(`${first_name} ${last_name}`)
+            setFollowingNum(following.length)
+            setProfilePic(profile_pic)
+            setUsername(username)
+          }
         })
-        .catch(console.log("Error"))
     }
     else {
       fetch(`http://127.0.0.1:8000/account/details`, {
@@ -67,44 +71,50 @@ function Profile() {
 
   return (
     <Fragment>
-      <Stack direction="column" alignItems="center">
-        <Avatar src={profilePic} sx={{ width: 120, height: 120 }} size='large' alt="Profile Image">
-          <Typography variant="h2">M</Typography>
-        </Avatar>
+      {
+        notFound
+          ? <NotFound message="User Not Found" />
+          : <Fragment>
+            <Stack direction="column" alignItems="center">
+              <Avatar src={profilePic} sx={{ width: 120, height: 120 }} size='large' alt="Profile Image">
+                <Typography variant="h2">M</Typography>
+              </Avatar>
 
-        <Typography mt fontWeight="bold" variant="h4">{fullName}</Typography>
-        <Typography>@{username}</Typography>
-        <Typography>{followingNum} following</Typography>
+              <Typography mt fontWeight="bold" variant="h4">{fullName}</Typography>
+              <Typography>@{username}</Typography>
+              <Typography>{followingNum} following</Typography>
 
-        <Stack direction="row" spacing={1} mt>
-          <Button disableElevation color="grey">Share</Button>
-          <Link to="/settings" className={classes.link}>
-            <Button disableElevation color="grey">Edit Profile</Button>
-          </Link>
-        </Stack>
-      </Stack>
+              <Stack direction="row" spacing={1} mt>
+                <Button disableElevation color="grey">Share</Button>
+                <Link to="/settings" className={classes.link}>
+                  <Button disableElevation color="grey">Edit Profile</Button>
+                </Link>
+              </Stack>
+            </Stack>
 
-      <Stack direction="row" justifyContent="space-between" mt={7}>
-        <MenuButton
-          icon={<MenuRoundedIcon fontSize="large" />}
-          label="Sort boards by"
-          options={["A to Z", "Drag and drop", "Last saved to"]}
-        />
+            <Stack direction="row" justifyContent="space-between" mt={7}>
+              <MenuButton
+                icon={<MenuRoundedIcon fontSize="large" />}
+                label="Sort boards by"
+                options={["A to Z", "Drag and drop", "Last saved to"]}
+              />
 
-        <MenuButton
-          icon={<AddRounded fontSize="large" />}
-          label="Create"
-          options={["Pin", "Board"]}
-        />
-      </Stack>
+              <MenuButton
+                icon={<AddRounded fontSize="large" />}
+                label="Create"
+                options={["Pin", "Board"]}
+              />
+            </Stack>
 
-      <Divider />
-      <Stack direction='row' justifyContent="space-between" mt={3}>
-        <Typography fontWeight="bold" variant="h6">Unorganized Ideas</Typography>
-        <Button color="grey">Organize</Button>
-      </Stack>
+            <Divider />
+            <Stack direction='row' justifyContent="space-between" mt={3}>
+              <Typography fontWeight="bold" variant="h6">Unorganized Ideas</Typography>
+              <Button color="grey">Organize</Button>
+            </Stack>
 
-      <HomePage />
+            <HomePage />
+          </Fragment>
+      }
     </Fragment>
   );
 }
