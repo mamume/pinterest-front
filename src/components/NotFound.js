d9fc69b1cb6185a7936b4e622751d73cb9250f59
mamume <mmustafametwally@gmail.com>
2021-11-27 14:46:50 +0200

    edit not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,11 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
-function ProfileNotFound({ message }) {
+function ProfileNotFound({ message, statusCode }) {
   return (
     <Box mt={20}>
-      <Typography variant="h1" textAlign="center" fontWeight="bold">404</Typography>
-      <Typography variant="h3" textAlign="center">Not Found</Typography>
-      <Typography variant="body1" textAlign="center">{message}</Typography>
+      <Typography variant="h1" textAlign="center" fontWeight="bold">{statusCode}</Typography>
+      <Typography variant="h3" textAlign="center">{message}</Typography>
     </Box>
   );
 }
