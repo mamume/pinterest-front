9795d604c5884116a7c46a0dd856cab19000f1a5
mamume <mmustafametwally@gmail.com>
2021-12-03 16:24:48 +0200

    remove comments

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -1,17 +1,3 @@
-// export default function LinkStyles() {
-//   return {
-//     position: 'absolute',
-//     top: '50%',
-//     left: '50%',
-//     transform: 'translate(-50%, -50%)',
-//     width: 445,
-//     bgcolor: 'background.paper',
-//     borderRadius: "32px",
-//     boxShadow: 24,
-//     p: 4
-//   }
-// }
-
 import { makeStyles } from "@mui/styles";
 
 
