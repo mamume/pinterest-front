9279b5a68bce98ba419e7453b142512ddd2587fc
mamume <mmustafametwally@gmail.com>
2021-12-03 19:04:55 +0200

    handle board privacy

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -24,6 +24,7 @@ function Board() {
   const [openCreatePin, setOpenCreatePin] = useState(false)
   const [ownerId, setOwnerId] = useState(null)
   const [isAuthedBoard, setIsAuthedBoard] = useState(false)
+  const [authorized, setAuthorized] = useState(true)
 
   const { headers, host, authedUser } = useContext(UserContext)
 
@@ -46,36 +47,44 @@ function Board() {
     }
     else
       setNotFound(true)
-  }, [boardId, headers, host])
+  }, [boardId, headers, host, authedUser.id])
 
   useEffect(() => {
-    title && setLoaded(true)
-  }, [title])
+    title && authedUser.id && setLoaded(true)
+  }, [title, authedUser.id])
 
   useEffect(() => {
     ownerId === authedUser.id && setIsAuthedBoard(true)
   }, [authedUser.id, ownerId])
 
+  useEffect(() => {
+    !isAuthedBoard && !share
+      ? setAuthorized(false)
+      : setAuthorized(true)
+  }, [isAuthedBoard, share])
+
   return (
     <Fragment>
+      {/* {console.log(authorized, isAuthedBoard, share)} */}
       {notFound
         ? <NotFound statusCode={400} message="Board is not found" />
         : loaded
-          ? (
-            <Fragment>
-              <Stack direction="column" alignItems="center">
-                <Avatar src={coverImage || '/images/board_placeholder.png'} sx={{ width: 120, height: 120 }} size='large' alt="Profile Image">
-                </Avatar>
-                {/* <Stack direction='row' alignItems="baseline" spacing> */}
-                <Typography mt fontWeight="bold" variant="h4">{title}</Typography>
-                {/* <MenuButton
+          ? authorized
+            ? (
+              <Fragment>
+                <Stack direction="column" alignItems="center">
+                  <Avatar src={coverImage || '/images/board_placeholder.png'} sx={{ width: 120, height: 120 }} size='large' alt="Profile Image">
+                  </Avatar>
+                  {/* <Stack direction='row' alignItems="baseline" spacing> */}
+                  <Typography mt fontWeight="bold" variant="h4">{title}</Typography>
+                  {/* <MenuButton
             icon={<MoreHorizIcon />}
             options={["Edit Board", "Share", "Merge", "Archive"]}
             label="Board Options"
           /> */}
-                {/* </Stack> */}
+                  {/* </Stack> */}
 
-                {/* <Button onClick={handleOpen} color="text" disableElevation sx={{ margin: 0, padding: 0, borderRadius: "16px" }}>
+                  {/* <Button onClick={handleOpen} color="text" disableElevation sx={{ margin: 0, padding: 0, borderRadius: "16px" }}>
           <Stack direction='row' alignItems="center">
             <AvatarGroup max={2}>
               <Avatar alt="Remy Sharp" src="#" />
@@ -88,7 +97,7 @@ function Board() {
           </Stack>
         </Button> */}
 
-                {/* <Modal
+                  {/* <Modal
                   open={open}
                   onClose={handleClose}
                 >
@@ -97,8 +106,8 @@ function Board() {
                   />
                 </Modal> */}
 
-                <Typography>{share ? "Public" : "Private"} Board</Typography>
-                {/* <Stack direction="row" spacing mt mb>
+                  <Typography>{share ? "Public" : "Private"} Board</Typography>
+                  {/* <Stack direction="row" spacing mt mb>
           <Stack alignItems="center">
             <IconButton sx={boardBtn}>
               <FlareRoundedIcon fontSize="large" color="black" />
@@ -120,47 +129,48 @@ function Board() {
             <Typography variant="caption">Notes</Typography>
           </Stack>
         </Stack> */}
-              </Stack>
+                </Stack>
 
-              <Divider sx={{ marginY: 5 }} />
+                <Divider sx={{ marginY: 5 }} />
 
-              <Stack direction="row" justifyContent="space-between">
-                <Typography fontWeight="bold">{pinItems.length} Pins</Typography>
-                {/* <Link className={classes.link} to={`/create_pin?board_id=${boardId}`}> */}
-                {isAuthedBoard && (
-                  <Fragment>
-                    <Button
-                      color="grey"
-                      onClick={() => setOpenCreatePin(true)}
-                    >
-                      Create Pin
-                    </Button>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography fontWeight="bold">{pinItems.length} Pins</Typography>
+                  {/* <Link className={classes.link} to={`/create_pin?board_id=${boardId}`}> */}
+                  {isAuthedBoard && (
+                    <Fragment>
+                      <Button
+                        color="grey"
+                        onClick={() => setOpenCreatePin(true)}
+                      >
+                        Create Pin
+                      </Button>
 
-                    <CreatePin
-                      open={openCreatePin}
-                      onClose={() => setOpenCreatePin(false)}
-                    />
-                  </Fragment>
-                )}
-                {/* </Link> */}
+                      <CreatePin
+                        open={openCreatePin}
+                        onClose={() => setOpenCreatePin(false)}
+                      />
+                    </Fragment>
+                  )}
+                  {/* </Link> */}
 
-                {/* <MenuButton
+                  {/* <MenuButton
           icon={<MenuRoundedIcon fontSize="large" />}
           label="Sort boards by"
           options={["A to Z", "Drag and drop", "Last saved to"]}
         /> */}
-              </Stack>
+                </Stack>
 
-              {Boolean(pinItems.length)
-                ? <Masonry style={{ width: "100%", paddingLeft: "80px" }}  >
-                  {pinItems.map((item) => (
-                    <SinglePin key={item.id} img={item.content_src} id={item.id} />
-                  ))}
-                </Masonry>
-                : <Typography textAlign="center">There aren’t any Pins on this board yet</Typography>
-              }
-            </Fragment>
-          )
+                {Boolean(pinItems.length)
+                  ? <Masonry style={{ width: "100%", paddingLeft: "80px" }}  >
+                    {pinItems.map((item) => (
+                      <SinglePin key={item.id} img={item.content_src} id={item.id} />
+                    ))}
+                  </Masonry>
+                  : <Typography textAlign="center">There aren’t any Pins on this board yet</Typography>
+                }
+              </Fragment>
+            )
+            : <NotFound message="Private Board" />
           : <Stack direction="row" justifyContent="center" mt={10}><CircularProgress /></Stack>
       }
     </Fragment >
