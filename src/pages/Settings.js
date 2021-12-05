4bd13adce4b89add602035b37a05ff87b4dee068
mamume <mmustafametwally@gmail.com>
2021-12-05 17:14:36 +0200

    add setting page margin

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -128,7 +128,7 @@ function Setting() {
             </ListItemButton> */}
           </List>
         </Drawer>
-        <Stack spacing={2} sx={{ width: "490px" }}>
+        <Stack mt={3} spacing={2} sx={{ width: "490px" }}>
           <Routes>
             <Route path="" element={<PublicProfile />}
             />
