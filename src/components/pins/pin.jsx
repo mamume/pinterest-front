8f04a3f241f7812200ae98383a595ffc68213535
mamume <mmustafametwally@gmail.com>
2021-12-06 14:25:56 +0200

    change useEffect dependencies

--- src/components/pins/pin.jsx
+++ src/components/pins/pin.jsx
@@ -40,14 +40,14 @@ const commentclick = () => {
 }
 
 
-const Pin = ({open, onClose, id, removeItem}) => {
+const Pin = ({ open, onClose, id, removeItem }) => {
     const [pin, setPin] = useState({})
     // let data = useLocation();
     let history = useNavigate();
     let param = useParams();
     const classes = Styles();
     const { authedUser, headers, host } = useContext(UserContext)
-    
+
 
     useEffect(() => {
         fetch(`${host}/pin/${id}`)
@@ -63,16 +63,15 @@ const Pin = ({open, onClose, id, removeItem}) => {
 
 
             })
-    }, [host,])
-
+    }, [authedUser, host, id])
     const handleDelete = () => {
         fetch(`${host}/profile/pins-delete/${id}/`, {
             headers,
             method: "DELETE"
-          })
+        })
             .then(res => res.json())
             .catch(data => {
-                
+
                 removeItem(id);
                 onClose();
             })
@@ -83,105 +82,105 @@ const Pin = ({open, onClose, id, removeItem}) => {
         <Modal
             open={open}
             onClose={onClose}
-            style={{zIndex:"90000000000000"}}
+            style={{ zIndex: "90000000000000" }}
         >
-            <Box className={classes.modal} sx={{width: "700px !important", maxHeight: "600px !important"}} >
-            <div className="container">
-                <div className="sides row">
+            <Box className={classes.modal} sx={{ width: "700px !important", maxHeight: "600px !important" }} >
+                <div className="container">
+                    <div className="sides row">
 
-                    <div className="left-side col-md-5">
-                        <div className="modals_pin_pin">
-                            <div className="pin_image_pin">
-                                <img src={pin.img} style={{height: "inherit", width: "inherit"}} alt="pin_image" />
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className="right-side col-md-5">
-                        <div className="section1 row">
-                            <div className="icons col-6">
-                                <div className="icon_more">
-                                    <i className="fas fa-ellipsis-h"></i>
+                        <div className="left-side col-md-5">
+                            <div className="modals_pin_pin">
+                                <div className="pin_image_pin">
+                                    <img src={pin.img} style={{ height: "inherit", width: "inherit" }} alt="pin_image" />
                                 </div>
-                                <div className="upload">
-                                    <i className="fas fa-upload"></i>
-                                </div>
-                                <div className="favorite">
-                                    <i className="far fa-star"></i>
-                                </div>
-                            </div>
-
-                            <div >
-                                {/* <div className="save_pin_pin"><span>delete</span></div> */}
-                                { (authedUser.id == pin.owner) && <Button  onClick ={handleDelete} variant = "outline" color= "primary" sx={{color: "white !important", backgroundColor: " #e33225 !important"}}>Delete</Button> }
-                                {/* <div className="save_pin_pin"><span>save</span></div> */}
                             </div>
 
                         </div>
-
-                        <div className="section2 row">
-                            <div className="section2_header col-12">
-                                <div className="pin_title_for_pin h1">{pin.title}</div>
-                                <div className="pin_description_for_pin">{pin.desc}</div>
-                            </div>
-                            <div className="user_details row">
-                                <div className="user_icon col-1"><span>m</span></div>
-                                <div className="user_name col-4"><span>momen awad</span></div>
-                            </div>
-
-                            <div className="note">
-                                <div className="note_head">
-                                    Note to self
-                                </div>
-                                <div className="note_details">
-                                    what do you want to remember about this pin?
-                                </div>
-                                <div className="add_note" ><span>Add note</span></div>
-                            </div>
-
-
-                            <div className="section3">
-                                <div className="comment_head">
-                                    <h3>comments</h3>
-                                    <div className="comment_icon" onClick={commentclick}>
-                                        <i className="fas fa-chevron-down"></i>
+                        <div className="right-side col-md-5">
+                            <div className="section1 row">
+                                <div className="icons col-6">
+                                    <div className="icon_more">
+                                        <i className="fas fa-ellipsis-h"></i>
+                                    </div>
+                                    <div className="upload">
+                                        <i className="fas fa-upload"></i>
+                                    </div>
+                                    <div className="favorite">
+                                        <i className="far fa-star"></i>
                                     </div>
                                 </div>
-                                <div className="comment_area_container" id="comment_area_container">
-                                    <div className="comment_area_header">
-                                        share feedback, ask a question or give a high five
-                                    </div>
 
-                                    <div className="comment_area">
-                                        <div className="persons_icon"><span>A</span></div>
-                                        <div className="text">
-                                            <input type="text" placeholder="Add a comment" name="comment" className="comment-btn" onFocus={handelfocus} onChange={handelchange} />
+                                <div >
+                                    {/* <div className="save_pin_pin"><span>delete</span></div> */}
+                                    {(authedUser.id == pin.owner) && <Button onClick={handleDelete} variant="outline" color="primary" sx={{ color: "white !important", backgroundColor: " #e33225 !important" }}>Delete</Button>}
+                                    {/* <div className="save_pin_pin"><span>save</span></div> */}
+                                </div>
+
+                            </div>
+
+                            <div className="section2 row">
+                                <div className="section2_header col-12">
+                                    <div className="pin_title_for_pin h1">{pin.title}</div>
+                                    <div className="pin_description_for_pin">{pin.desc}</div>
+                                </div>
+                                <div className="user_details row">
+                                    <div className="user_icon col-1"><span>m</span></div>
+                                    <div className="user_name col-4"><span>momen awad</span></div>
+                                </div>
+
+                                <div className="note">
+                                    <div className="note_head">
+                                        Note to self
+                                    </div>
+                                    <div className="note_details">
+                                        what do you want to remember about this pin?
+                                    </div>
+                                    <div className="add_note" ><span>Add note</span></div>
+                                </div>
+
+
+                                <div className="section3">
+                                    <div className="comment_head">
+                                        <h3>comments</h3>
+                                        <div className="comment_icon" onClick={commentclick}>
+                                            <i className="fas fa-chevron-down"></i>
                                         </div>
                                     </div>
-                                    <div className="comment_controllers" id="comment_controllers">
-                                        <input type="button" value="Cancel" name="comment_canceled" className="comment_canceled" onClick={handelclick} />
-                                        <input type="button" value="Done" name="comment_done" className="comment_done" disabled />
+                                    <div className="comment_area_container" id="comment_area_container">
+                                        <div className="comment_area_header">
+                                            share feedback, ask a question or give a high five
+                                        </div>
+
+                                        <div className="comment_area">
+                                            <div className="persons_icon"><span>A</span></div>
+                                            <div className="text">
+                                                <input type="text" placeholder="Add a comment" name="comment" className="comment-btn" onFocus={handelfocus} onChange={handelchange} />
+                                            </div>
+                                        </div>
+                                        <div className="comment_controllers" id="comment_controllers">
+                                            <input type="button" value="Cancel" name="comment_canceled" className="comment_canceled" onClick={handelclick} />
+                                            <input type="button" value="Done" name="comment_done" className="comment_done" disabled />
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div className="bord_name">
+                                <div className="bord_items">
+                                    <div className="comment_icon"><span>M</span></div>
+                                    <div className="bord_content">
+                                        <span>you saved to bord name</span>
                                     </div>
                                 </div>
                             </div>
 
 
+
+
                         </div>
-                        <div className="bord_name">
-                            <div className="bord_items">
-                                <div className="comment_icon"><span>M</span></div>
-                                <div className="bord_content">
-                                    <span>you saved to bord name</span>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-
                     </div>
                 </div>
-            </div>
 
             </Box>
 
